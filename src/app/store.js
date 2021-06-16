bbae257024d5b1b59590a23993a0dc51853b3137
Robert Engel <robert.engel@apollidon.com>
2021-06-16 16:34:45 -0400

    initial commit

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import postReducer  from '../features/posts/postsSlice'
+
+export default configureStore({
+  reducer: {
+    posts: postReducer
+  }
+})
